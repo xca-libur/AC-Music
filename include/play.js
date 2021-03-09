b8476a27aa6c44d5298094ae4771d277ac783c90
xca-libur <79561225+xca-libur@users.noreply.github.com>
2021-03-09 13:33:27 +0600

    Update play.js

--- include/play.js
+++ include/play.js
@@ -84,7 +84,7 @@ module.exports = {
 
     try {
       var playingMessage = await queue.textChannel.send(
-        i18n.__mf("play.startedPlaying", { title: song.title, url: song.url })
+        i18n.__mf("play.startedPlaying", { title: song.title })
       );
       await playingMessage.react("⏭");
       await playingMessage.react("⏯");
